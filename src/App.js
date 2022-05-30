ba7ee3e7f12a9ec2fa53226de5ace6d464926e20
clldaxigua <clldaxigua@126.com>
2022-05-30 20:48:37 +0800

    Succeed to connect to Metamask

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import Connect, {ConnectChain} from "./requests/connect/connect";
+import {useEagerConnect} from "./requests/connect/hook";
+import {ChainId} from "./requests/connect/connect";
+
+function getLibrary(provider) {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 5000
+  return library
+}
 
 function App() {
+  const triedEager = useEagerConnect()
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        This is Header.
       </header>
+      <div>
+        This is Content.
+      </div>
+      <div>
+        <Connect />
+      </div>
     </div>
   );
 }
---
+++ src/requests/connect/connect.js
@@ -0,0 +1,108 @@
+import React from 'react'
+import { Spinner } from './Spinner'
+import { injected } from './connectors'
+
+import { Web3ReactProvider, useWeb3React, } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import { useEagerConnect, useInactiveListener } from './hook'
+import './Connect.css';
+
+function getLibrary(provider) {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 5000
+  return library
+}
+
+function ChainId() {
+  const { chainId, library } = useWeb3React()
+
+  return (
+    <div className="ChainIdWrapper">
+      <span>Chain Id</span>
+      <span role="img" aria-label="chain">
+        ⛓
+      </span>
+      <span className="ChainIdText">{chainId ?? 'Not Connected'}</span>
+    </div>
+  )
+}
+
+function Connect() {
+  // const triedEager = useEagerConnect()
+
+  return (
+    <div>
+      <ChainId/>
+      <p>
+        Current value: n/a
+      </p>
+      <ConnectChain triedEager={true} />
+    </div>
+  );
+}
+
+
+export default function() {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Connect />
+    </Web3ReactProvider>
+  )
+}
+
+function ConnectChain(props) {
+  const context = useWeb3React()
+  const { connector, library, chainId, account, activate, deactivate, active, error } = context
+
+  const [activatingConnector, setActivatingConnector] = React.useState()
+  React.useEffect(() => {
+    if (activatingConnector && activatingConnector === connector) {
+      setActivatingConnector(undefined)
+    }
+  }, [activatingConnector, connector])
+
+  const activating = injected === activatingConnector
+  const connected = injected === connector
+  const disabled = !props.triedEager || !!activatingConnector || !!error
+
+  useInactiveListener(!props.triedEager || !!activatingConnector)
+
+  let isDisconnect = !error && chainId
+  const buttonText = isDisconnect ? 'Disconnect' : (activating ? 'Connecting' : 'Connect' )
+
+  return (
+    <button
+      style={{
+        borderColor: activating ? 'orange' : connected ? 'green' : 'unset',
+        cursor: disabled ? 'unset' : 'pointer',
+        position: 'relative',
+      }}
+      className="ConnectButton"
+      disabled={disabled}
+      onClick={() => {
+        if (!isDisconnect) {
+          setActivatingConnector(injected)
+          activate(injected)
+        } else {
+          deactivate()
+        }
+      }}
+    >
+      <div
+        style={{
+          position: 'absolute',
+          top: '0',
+          left: '0',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          color: 'black',
+          margin: '0 0 0 1rem'
+        }}
+      >
+        {activating && <Spinner color={'red'} style={{ height: '50%', marginLeft: '-1rem' }} />}
+      </div>
+      { buttonText }
+    </button>
+  )
+}
